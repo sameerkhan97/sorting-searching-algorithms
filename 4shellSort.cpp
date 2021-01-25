eb2e2c810706de82a33448fbb0cd9bea0f6a5ee2
sameerkhan97 <74293949+SameerKhan97@users.noreply.github.com>
2021-01-25 22:02:47 +0530

    Update 4shellSort.cpp

--- 4shellSort.cpp
+++ 4shellSort.cpp
@@ -2,22 +2,21 @@
 using namespace std;
 void display(int *a, int n){
     int i;
-    for(i=0;i<n;i++){
+    for(i=0;i<n;i++)
+    {
         cout<<a[i]<<" ";
     }
 }
-
 void swap(int &a, int &b){
     int temp;
     temp=a;
     a=b;
     b=temp;
 }
-
 void shellSort(int *a, int n){
     int i,gap=n/2, j;
-    
-    while(gap>0){
+    while(gap>0)
+    {
         for(j=gap;j<n;j++)
         {
             for(i=j-gap;i>=0;i=i-gap) 
@@ -31,8 +30,7 @@ void shellSort(int *a, int n){
      gap=gap/2;
      }
  }
-
- int main() {
+int main(){
     int i,size;
     cout<<"\nEnter Size of Array ";
     cin>>size;
@@ -44,13 +42,10 @@ void shellSort(int *a, int n){
     }
     cout<<"\nBefore Sorting : ";
     display(arr, size);
-    
     shellSort(arr, size);
-    
     cout<<"\nAfter Sorting : ";
     display(arr, size);
     return 0;
-    
 }
     
     
