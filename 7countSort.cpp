593783bd944439339f451545f269222c84672dc6
sameerkhan97 <74293949+SameerKhan97@users.noreply.github.com>
2021-01-25 22:13:47 +0530

    Update 7countSort.cpp

--- 7countSort.cpp
+++ 7countSort.cpp
@@ -2,50 +2,39 @@
 using namespace std;
 void display(int *a, int n) {
     int i;
-    for(i=0;i<n;i++){
-    cout<<a[i]<<" ";
-    }
+    for(i=0;i<n;i++)
+        cout<<a[i]<<" ";
 }
 void countSort(int *a, int n, int k){
     int i, count[k+1],b[n];
-    for(i=0;i<=k;i++){
+    for(i=0;i<=k;i++)
         count[i]=0;
-    }
-    for(i=0;i<n;i++){
+    for(i=0;i<n;i++)
         count[a[i]]++;
-    }
-    for(i=1;i<=k;i++){
+    for(i=1;i<=k;i++)
         count[i]=count[i]+count[i-1];
-    }
-    for(i=n-1;i>=0;i--){
+    for(i=n-1;i>=0;i--)
         b[--count[a[i]]]=a[i];
-    }
-    for(i=0;i<n;i++){
+    for(i=0;i<n;i++)
         a[i]=b[i];
-    }
 }
-
 int main()
 {
     int n, i, max=0;
     cout<<"\nEnter Number of Elements ";
     cin>>n;
     int a[n];
-    
     cout<<"\nEnter "<<n<<" Elements ";
-    for(i=0;i<n;i++){
-    cin>>a[i];
-    if(a[i]>max)
-        max=a[i];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+        if(a[i]>max)
+            max=a[i];
     }
-    
     cout<<"\nBefore Sorting : ";
-    display(a, n) ;
-   
-    countSort(a, n, max) ;
-    
+    display(a,n) ;
+    countSort(a,n,max) ;
     cout<<"\nAfter Sorting :  ";
-    display(a, n);
-    
+    display(a,n);
     return 0;
  }
