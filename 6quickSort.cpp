9f7d61647fff9a3b039534a7ff9840fb52a1447b
sameerkhan97 <74293949+SameerKhan97@users.noreply.github.com>
2021-01-25 22:10:46 +0530

    Update 6quickSort.cpp

--- 6quickSort.cpp
+++ 6quickSort.cpp
@@ -8,30 +8,26 @@ void swap(int &a, int &b) {
 }
 void display(int *a, int n) {
     int i;
-    for(i=0;i<n;i++){
-    cout<<a[i]<<" ";
+    for(i=0;i<n;i++)
+    {
+        cout<<a[i]<<" ";
     }
 }
-int partition(int *a,int l,int u)
- {
+int partition(int *a,int l,int u){
      int start=l,end=u,pivot=a[l];
-    while(start<end) 
-    {
-        while(a[start]<=pivot){
+     while(start<end) 
+     {
+        while(a[start]<=pivot)
             start++;
-        }
-        while(a[end]>pivot) {
+        while(a[end]>pivot)
             end--;
-        }
         if(start<end) 
             swap(a[start],a[end]) ;
-    }
-        swap(a[l], a[end]) ;
-
-    return end;
+     }
+     swap(a[l], a[end]) ;
+     return end;
  }
- void quickSort(int *a, int l, int u)   
- {
+ void quickSort(int *a, int l, int u){
     int loc;
     if(l<u) 
     {
@@ -39,25 +35,21 @@ int partition(int *a,int l,int u)
         quickSort(a, l, loc-1) ;
         quickSort(a, loc+1, u) ;
      }
- }   
-int main()
-{
+}   
+int main(){
     int n, i;
     cout<<"\nEnter Number of Elements ";
     cin>>n;
     int a[n];
-    
     cout<<"\nEnter "<<n<<" Elements ";
-    for(i=0;i<n;i++){
-    cin>>a[i];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
     }
     cout<<"\nBefore Sorting : ";
     display(a, n) ;
-   
     quickSort(a, 0,n-1) ;
-    
     cout<<"\nAfter Sorting :  ";
     display(a, n);
-    
     return 0;
  }
