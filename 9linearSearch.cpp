a99957010c976c5e69f2818127cf3fb27d8b2b5c
sameerkhan97 <74293949+SameerKhan97@users.noreply.github.com>
2021-01-25 22:18:31 +0530

    Update 9linearSearch.cpp

--- 9linearSearch.cpp
+++ 9linearSearch.cpp
@@ -1,16 +1,13 @@
 #include<iostream>
 using namespace std;
-int main()
-{
+int main(){
     int n, i, b, c=0;
     cout<<"\nEnter Number of Elements ";
     cin>>n;
     int a[n];
-    
     cout<<"\nEnter "<<n<<" Elements ";
-    for(i=0;i<n;i++){
-    cin>>a[i];
-    }
+    for(i=0;i<n;i++)
+        cin>>a[i];
     cout<<"\nEnter element to search ";
     cin>>b;
     for(i=0;i<n;i++) 
@@ -20,9 +17,8 @@ int main()
             cout<<"\n"<<b<<" found at index "<<i;
             c++;
         }
-     }
+    }
     if(c==0)
         cout<<"\nElement not found ";
- 
     return 0;
  }
