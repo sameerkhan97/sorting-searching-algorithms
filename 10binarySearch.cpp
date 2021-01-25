8425089858c9ea91851989bb88272d8f67edaff3
sameerkhan97 <74293949+SameerKhan97@users.noreply.github.com>
2021-01-25 21:55:11 +0530

    Update 10binarySearch.cpp

--- 10binarySearch.cpp
+++ 10binarySearch.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 void selectSort(int *a, int n){
     int i, j, min, loc;
-    for(i=0;i<n;i++){
-    min=a[i];
-    loc=i;
+    for(i=0;i<n;i++)
+    {
+        min=a[i];
+        loc=i;
         for(j=i;j<n;j++)
         {
             if(a[j]<min)
@@ -17,44 +18,32 @@ void selectSort(int *a, int n){
         temp=a[i];
         a[i]=a[loc];
         a[loc]=temp;
-     }
-   
+   }
 }
-             
-
-
-
-
-
 int binarySearch(int *a, int l, int u, int s){
     int mid;
     if(l<=u){
-    mid=(l+u)/2;
-    if(a[mid]==s)
-        return mid;
-    if(a[mid]>s)
-       return binarySearch(a, l, mid-1, s);
-    if(a[mid]<s)
-       return binarySearch(a, mid+1,u, s);
+        mid=(l+u)/2;
+        if(a[mid]==s)
+            return mid;
+        if(a[mid]>s)
+            return binarySearch(a, l, mid-1, s);
+        if(a[mid]<s)
+            return binarySearch(a, mid+1,u, s);
     }
-      return -1;
+     return -1;
 }
-int main()
-{
+int main(){
     int n, i, s,c;
     cout<<"\nEnter Number of Elements ";
     cin>>n;
     int a[n];
-    
     cout<<"\nEnter "<<n<<" Elements ";
-    for(i=0;i<n;i++){
-    cin>>a[i];
-    }
+    for(i=0;i<n;i++)
+        cin>>a[i];
     selectSort(a, n);
-    
     cout<<"\nEnter element to search ";
     cin>>s;
-    
     c=binarySearch(a, 0,n-1,s);
     if(c==-1)
         cout<<"\nElement not found ";
