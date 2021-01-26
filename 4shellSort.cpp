eea11cccf47810f6a7ff8f6334b1a484a2132fbf
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-26 11:36:46 +0530

    Update 4shellSort.cpp

--- 4shellSort.cpp
+++ 4shellSort.cpp
@@ -35,8 +35,7 @@ int main(){
     cin>>size;
     int arr[size];
     cout<<"\nEnter elements ";
-    for(i=0;i<size;i++) 
-    {
+    for(i=0;i<size;i++) {
         cin>>arr[i];
     }
     cout<<"\nBefore Sorting : ";
