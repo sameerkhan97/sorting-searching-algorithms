b25133f1903bbbc4d16d2e5537246e724226525d
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-27 21:33:15 +0530

    Code Description Added

--- 8redixSort.cpp
+++ 8redixSort.cpp
@@ -7,21 +7,21 @@ void display(int *a, int n) {
     }
 }
 void countSort(int *a, int n, int pos){
-    int i, count[10],b[n];
+    int i, count[10],b[n];  //defining count array of size 10 bcoz digit vary from 0-9
     for(i=0;i<10;i++)
         count[i]=0;
     for(i=0;i<n;i++)
-        count[(a[i]/pos)%10]++;
-    for(i=1;i<10;i++)
-        count[i]=count[i]+count[i-1];
+        count[(a[i]/pos)%10]++; //value at a[i] will be devided and particular digit of value will be used as an index in count incrementing it to 1. 
+    for(i=1;i<10;i++)           
+        count[i]=count[i]+count[i-1]; //values at count array will be incremented by adding previous element values.
     for(i=n-1;i>=0;i--)
-        b[--count[(a[i]/pos)%10 ]]=a[i];
-    for(i=0;i<n;i++)
-        a[i]=b[i];
+        b[--count[(a[i]/pos)%10 ]]=a[i];//value at a[i] will be devided and particular digit of value will be used as an index in count ....
+    for(i=0;i<n;i++)                    //value at that index of count will be decremented by one and used as index of array b,value at ith index in a will be copied at that index of b.
+        a[i]=b[i];                       
 }
 void radixSort(int *a, int n, int max){
-    int pos;
-    for(pos=1;max/pos>0;pos*10)
+    int pos;    //pos defines which digit to be compared in sorting using (max/pos)%10,like if pos=1 then sorting will be on leftmost digit,if pos=10 then second...
+    for(pos=1;max/pos>0;pos*10) //second leftmost digit will be used for sorting
         countSort(a,n,pos);
 }
 int main(){
