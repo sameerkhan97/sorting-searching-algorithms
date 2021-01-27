b51d9143364167e28f7b1d0ee44a696bf3475f21
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-27 16:20:10 +0530

    code description added

--- 1bubleSort.cpp
+++ 1bubleSort.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+// function using reference variable and swap two values
 void swap(int &a, int &b) {
     int temp;
     temp=a;
@@ -14,14 +15,15 @@ void display(int *a, int n) {
 }
 void bubleSort(int *a, int n){
     int c=1,i;
+    //c will count the number of time array is checked and while loop will operate untill c is smaller than array size.
      while(c<n) 
      {
         for(i=0;i<n;i++)
         {
-            if(a[i]>a[i+1])
+            if(a[i]>a[i+1]) //Element at ith index will be compared with next element and if found smaller then will be swapped with next one.
                 swap(a[i], a[i+1]);
         }
-      c++;
+     c++;
     } 
 }
 int main()
