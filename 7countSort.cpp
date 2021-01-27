4c82965b0f71c85f90b0693d4f0bb276bec1aa65
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-27 20:22:07 +0530

    Code Description Added

--- 7countSort.cpp
+++ 7countSort.cpp
@@ -6,17 +6,17 @@ void display(int *a, int n) {
         cout<<a[i]<<" ";
     }
 }
-void countSort(int *a, int n, int k){
-    int i, count[k+1],b[n];
-    for(i=0;i<=k;i++)
+void countSort(int *a, int n, int k){//k-store max element in array,n-size of array a.
+    int i, count[k+1],b[n];         //cout array of size k will be created to access index of array b(b has same size as array a)
+    for(i=0;i<=k;i++)              
         count[i]=0;
     for(i=0;i<n;i++)
-        count[a[i]]++;
+        count[a[i]]++;              //count array element will be incremented to 1 untill index of (n-1) ,elements after (n-1) will remain 0. 
     for(i=1;i<=k;i++)
-        count[i]=count[i]+count[i-1];
-    for(i=n-1;i>=0;i--)
-        b[--count[a[i]]]=a[i];
-    for(i=0;i<n;i++)
+        count[i]=count[i]+count[i-1]; //values in count array will incremented like 0th index storing 1,1st index storing 2,2nd index storing 3 till (n-1) but....
+    for(i=n-1;i>=0;i--)               //after (n-1)th all indices will store only n bcoz they earlier had 0,which will not increment their value.
+        b[--count[a[i]]]=a[i];     //element in array a @ ith index will be refered in count's index and its value will be decremented and that index...
+    for(i=0;i<n;i++)               //will be accessed in array b and array a's value at ith index will be copied thier.
         a[i]=b[i];
 }
 int main()
