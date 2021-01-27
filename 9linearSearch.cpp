c0e67437bd46f23ca71528263b88addaf95abfbb
sameer khan <74293949+sameerkhan97@users.noreply.github.com>
2021-01-27 16:45:26 +0530

    Code Description Added

--- 9linearSearch.cpp
+++ 9linearSearch.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n, i, b, c=0;
+    int n, i, b, c=0;   //initialising c with 0 ,so that once we found the element we will increment it which will help in the end condition.
+                        //Using c we can also count occurance of a element in array bcoz every time an element is found c will be incremented by one
     cout<<"\nEnter Number of Elements ";
     cin>>n;
     int a[n];
@@ -13,10 +14,10 @@ int main(){
     cin>>b;
     for(i=0;i<n;i++) 
     {
-        if(a[i]==b)
-        {
+        if(a[i]==b) //all the array elements will be one by one compared with searching element
+        {                                   
             cout<<"\n"<<b<<" found at index "<<i;
-            c++;
+            c++;    //incrementing count will be usefull at end to know whether element is present or not
         }
     }
     if(c==0)
